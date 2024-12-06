3210c41a702b78e8472102849568526533574d69
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-12-06 15:18:23 +0200

    write urls for views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,22 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from cinema.views import movie_list, movie_detail
-
-urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-]
+from .views import GenreAPIView, ActorGenericAPIView, CinemaHallViewSet, MovieViewSet
 
 app_name = "cinema"
+
+cinema_hall_router = DefaultRouter()
+cinema_hall_router.register(r"cinema_halls", CinemaHallViewSet)
+
+movie_router = DefaultRouter()
+movie_router.register(r"movies", MovieViewSet)
+
+urlpatterns = [
+    path("genres/", GenreAPIView.as_view(), name="genre-list-create"),
+
+    path("actors/", ActorGenericAPIView.as_view(), name="actor-list-create"),
+
+    path("", include(cinema_hall_router.urls)),
+
+    path("", include(movie_router.urls)),
+]
